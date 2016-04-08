bd5831cfbf545f52e3fcd625abd7bbecc908d737
kawasaki tatsuya <ta2ya.xxx@gmail.com>
2016-04-08 19:04:18 +0900

    add express

--- app.js
+++ app.js
@@ -1,33 +1,13 @@
-var https = require('https');
+var express = require('express')
+var app = express()
 
-exports.handler = function(event, context) {
-    console.log('Received event:', JSON.stringify(event, null, 2));
-    var msg = event.result[0];
-    var data = JSON.stringify({
-      to: [msg.content.from.toString()],
-      toChannel: 1383378250,
-      eventType: "138311608800106203",
-      content: msg.content
-    });
-    var url ='https://trialbot-api.line.me/v1/events';
-    var opts = {
-        host: 'trialbot-api.line.me',
-        path: '/v1/events',
-        headers: {
-            "Content-type": "application/json; charset=UTF-8",
-            "X-Line-ChannelID": "1462107785",
-            "X-Line-ChannelSecret": "b669d183696c3c503c533ab4bcf94e71",
-            "X-Line-Trusted-User-With-ACL": "u5d5db3fd902b6c9a776d1fe8f8028317"
-        },
-        method: 'POST'
-    }
-    var req = https.request(opts, function(res){
-        res.on('data', function(chunk){
-            console.log(chunk.toString())
-        }).on('error', function(e){
-            console.log('ERROR: '+ e.stack);
-        })
-    })
-    req.write(data)
-    req.end();
-};
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
